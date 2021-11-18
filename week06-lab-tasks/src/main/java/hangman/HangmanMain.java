52a9c87a662031fc85923e2284189f5fa323039d
deekev <deekev@gmail.com>
2021-11-18 16:18:56 +0100

    hangman

---
+++ week06-lab-tasks/src/main/java/hangman/HangmanMain.java
@@ -0,0 +1,47 @@
+package hangman;
+
+import java.util.Scanner;
+
+public class HangmanMain {
+
+    private String wordToFind = "alma";
+    private String wordFound = "____";
+    private int chances = 8;
+
+    public static void main(String[] args) {
+        new HangmanMain().run();
+    }
+
+    private void run() {
+        Scanner scanner = new Scanner(System.in);
+        String guess = "";
+        do{
+            System.out.println("Itt tartasz: " + wordFound);
+            System.out.println("Ennyi tipped van: " + chances);
+            System.out.println("Mi a következő tipped?");
+            guess = scanner.nextLine();
+            if (wordToFind.contains(guess)) {
+                wordFound = updateWordFound(wordToFind, wordFound, guess);
+            } else {
+                chances--;
+            }
+        } while (!wordFound.equals(wordToFind) && chances > 0);
+
+        if (wordFound.equals(wordToFind)) {
+            System.out.println(wordFound);
+            System.out.println("Ügyes vagy.");
+        } else {
+            System.out.println("Vesztettél.");
+        }
+    }
+
+    private String updateWordFound(String wordToFind, String status, String guess) {
+        char[] chars = status.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (wordToFind.charAt(i) == guess.charAt(0)) {
+                chars[i] = guess.charAt(0);
+            }
+        }
+        return new String(chars);
+    }
+}
